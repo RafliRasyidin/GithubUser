c14c7a04028ccaebc2b1a3538fdea65f1dfa64cc
RafliRasyidin <raflirasyidin20@gmail.com>
2020-07-22 00:13:25 +0700

    Add UNIQUE attribute of $USERNAME

--- app/src/main/java/com/rasyidin/githubuser/database/DatabaseContract.kt
+++ app/src/main/java/com/rasyidin/githubuser/database/DatabaseContract.kt
@@ -8,18 +8,18 @@ object DatabaseContract {
     const val AUTHORITY = "com.rasyidin.githubuser"
     const val SCHEME = "content"
 
-    class FavoriteColumns: BaseColumns {
+    class FavoriteColumns : BaseColumns {
         companion object {
             const val TABLE_NAME = "favorite"
             const val _ID = "_id"
             const val USERNAME = "username"
             const val AVATAR_URL = "avatar_url"
             const val TYPE = "type"
-
-            val CONTENT_URI: Uri = Uri.Builder().scheme(SCHEME)
-                .authority(AUTHORITY)
-                .appendPath(TABLE_NAME)
-                .build()
         }
     }
+
+    val CONTENT_URI: Uri = Uri.Builder().scheme(SCHEME)
+        .authority(AUTHORITY)
+        .appendPath(FavoriteColumns.TABLE_NAME)
+        .build()
 }
